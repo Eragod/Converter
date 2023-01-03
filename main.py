7d244ff3855a42315acd61e00cf14f7c21cc7fd7
Eragod <120926143+Eragod@users.noreply.github.com>
2023-01-03 16:21:51 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import customtkinter
 from PIL import Image
 
+
+converter = "Converter"
+
+
 customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("green")  # Themes: blue (default), dark-blue, green
 
@@ -22,6 +26,11 @@ def convert_to_png():
     img.save("output.png")
 
 
+def convert_to_ico():
+    img = Image.open(path.get())
+    img.save("Converter.ico")
+
+
 app = customtkinter.CTk()
 app.title("Converter")
 app.geometry("500x350")
@@ -39,6 +48,9 @@ path.pack(pady=10)
 
 
 btn_png = customtkinter.CTkButton(Tabview.tab("Converter"), text="Convert to .png", command=convert_to_png)
+btn_png.pack(pady=10)
+btn_ico = customtkinter.CTkButton(Tabview.tab("Converter"), text="Convert to .ico", command=convert_to_ico)
+btn_ico.pack(pady=10)
 
 
 change_app_mode = customtkinter.CTkOptionMenu(Tabview.tab("Settings"), values=["System", "Dark", "Light"],
