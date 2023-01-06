249f1657d04cf1a16f9ac107a368ef399b818b80
Eragod <120926143+Eragod@users.noreply.github.com>
2023-01-06 16:05:51 +0600

    0.1.0

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 # imports
-import customtkinter
-from PIL import Image
+import customtkinter  # Custom tkinter
+from PIL import Image  # Pillow
+import webbrowser  # Web browser
 
-# Themes
+# Themes / Modes
 customtkinter.set_appearance_mode("System")  # Modes: system, light, dark
 customtkinter.set_default_color_theme("green")  # Themes: blue, dark-blue, green
 
 
 # Functions
 def change_app_mode_event(new_app_mode: str):
-    customtkinter.set_appearance_mode(new_app_mode)
+    customtkinter.set_appearance_mode(new_app_mode)  #
 
 
 def change_language_event(language: str):
@@ -22,16 +23,26 @@ def change_language_event(language: str):
 
 def convert_to_png():
     img = Image.open(path.get())
-    img.save("output.png")
+    img.save("Saves/output.png")
 
 
 def convert_to_ico():
     img = Image.open(path.get())
-    img.save("Converter.ico")
+    img.save("Saves/output.ico")
+
+
+def convert_to_jpg():
+    img = Image.open(path.get())
+    img.save("Saves/output.jpg")
+
+
+def open_github():
+    webbrowser.open_new("https://github.com/Eragod/Converter")
 
 
 # App create
 app = customtkinter.CTk()
+app.iconbitmap("Images/Converter.ico")
 app.title("Converter")
 app.geometry("500x350")
 app.resizable(False, False)
@@ -44,7 +55,7 @@ Tabview.add("About the creator")
 Tabview.pack()
 
 # Path entry
-path = customtkinter.CTkEntry(Tabview.tab("Converter"), width=475)
+path = customtkinter.CTkEntry(Tabview.tab("Converter"), width=470)
 path.pack(pady=10)
 
 # Convert Buttons
@@ -52,18 +63,25 @@ btn_png = customtkinter.CTkButton(Tabview.tab("Converter"), text="Convert to .pn
 btn_png.pack(pady=10)
 btn_ico = customtkinter.CTkButton(Tabview.tab("Converter"), text="Convert to .ico", command=convert_to_ico)
 btn_ico.pack(pady=10)
+btn_jpg = customtkinter.CTkButton(Tabview.tab("Converter"), text="Convert to .jpg", command=convert_to_jpg)
+btn_jpg.pack(pady=10)
 
 # Settings
+change_app_mode_text = customtkinter.CTkLabel(Tabview.tab("Settings"), text="Change appearance mode:").pack(pady=10)
 change_app_mode = customtkinter.CTkOptionMenu(Tabview.tab("Settings"), values=["System", "Dark", "Light"],
-                                              command=change_app_mode_event).pack(pady=10)
+                                              command=change_app_mode_event).pack()
+change_language_text = customtkinter.CTkLabel(Tabview.tab("Settings"), text="Change language:").pack(pady=10)
 change_language = customtkinter.CTkOptionMenu(Tabview.tab("Settings"), values=["English", "Russian"],
-                                              command=change_language_event).pack(pady=10)
+                                              command=change_language_event).pack()
 
 # About
-about_text = customtkinter.CTkTextbox(Tabview.tab("About the creator"), width=475, height=295)
-about_text.pack()
-about_text.insert("0.0", "This application is created by Eragod1337 or Eragod \n"
-                         "Github - https://github.com/Eragod/Converter")
+about_text = customtkinter.CTkLabel(Tabview.tab("About the creator"),
+                                    text="This application is created by Eragod1337 . . . \n"
+                                         "Github - https://github.com/Eragod/Converter\n "
+                                         "This application is open source").pack(pady=10)
+
+open_repository = customtkinter.CTkButton(Tabview.tab("About the creator"), text="Open github repository!",
+                                          command=open_github).pack(pady=10)
 
 # Start!
 app.mainloop()
